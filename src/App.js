d027e7909c9931097392f8029a34e55f8e4775d3
hudaman0621 <mrhu828@gmail.com>
2020-06-08 14:10:54 -0400

    example

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import Card from './components/Cards';
 import './App.css';
 
 function App() {
+  const projects = [
+    { name: 'Project Awesome', likes: 10 },
+    { name: 'Project a2', likes: 10000000 },
+    { name: 'Project awe3', likes: 3 },
+    { name: 'Project awe 4', likes: 5 },
+    { name: 'BreafastTime', likes: 8 },
+    { name: 'Pan Fried Noodles', likes: 9 },
+    { name: 'Dinner', likes: 4 },
+    { name: 'Ham Fried rice', likes: 30000 },
+
+
+  ];
+
   return (
     <div className="App">
-      <Card name ="test 1" likes={ 0 }></Card>
-      <Card name ="test 2"></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-
+      {projects.map((project, index) => {
+        return <Card
+          name={project.name}
+          key={index}
+          likes={project.likes}
+        ></Card>
+      })}
     </div>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react' ;
 import './Card.css';
+import numeral from 'numeral'; 
+
 
 class Card extends Component {
     render() {
         return (
+
             <div className="Card">
                 { this.props.name }
                 <br></br>
-                { this.props.likes >= 0 ?( //this is how you do if statements
-                    <sup>likes: { this.props.likes } </sup>
+                { this.props.likes ?( //this is how you do if statements
+                    <sup>Likes: { numeral(this.props.likes).format('0.0a')} </sup>
                 ) : ''}
             </div>
         )
